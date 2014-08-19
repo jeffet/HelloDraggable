8068e476b9aa5033de62c43ef388154b8dbb6c18
Jeff Tenn <mobileFirstDemo1@gmail.com>
2014-08-18 21:29:57 -0400

    adding eclipse code

---
+++ HelloDraggable/apps/HelloDraggable/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3810785299,"date":1407893264197,"machine":"jtenmac.ibm.com"};
+/* Date: Tue Aug 12 21:27:44 EDT 2014 */
---
+++ HelloDraggable/apps/HelloDraggable/android/native/gen/com/HelloDraggable/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.HelloDraggable;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
